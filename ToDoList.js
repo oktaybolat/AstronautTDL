c59c1f1200dda7c829888d100d93de2c162470bb
Oktay Bolat <oktay.bolat@gmail.com>
2022-07-23 15:58:42 +0300

    addNewfunction notes

--- ToDoList.js
+++ ToDoList.js
@@ -1,8 +1,27 @@
+
+
+let toDoItems = [];
+
+// function to add  new item and necessary buttons into the list
 function addNewFunction(){
-   
-    const input = document.getElementById("js-ToDo-input");
+    
+  // get data from "myinput" text box
+  let input = document.getElementById("js-ToDo-input").value;
+  
+  //get data from ul
+
+
+  //push new data into list
+
+  // push radio, edit and delete buttons into the list
+
+
+
+
     alert(input);
+
+
 }
-addNewFunction()
+
 
 
