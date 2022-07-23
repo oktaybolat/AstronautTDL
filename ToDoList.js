d032ec523433b360fd60a4d139e7c90eae188165
Carlos Ferraz <carloslf@e.email>
2022-07-23 18:42:49 +0300

    updated js ans html

--- ToDoList.js
+++ ToDoList.js
@@ -1,16 +1,29 @@
 
-
+// Here is our list of ToDos that have been entered into our list
 let toDoItems = [];
 
+const submitButton = document.getElementById('js-adding-todo')
+submitButton.addEventListener('click', addNewFunction)
 // function to add  new item and necessary buttons into the list
 function addNewFunction(){
     
   // get data from "myinput" text box
-  let input = document.getElementById("js-ToDo-input").value;
+  const input = document.getElementById("js-ToDo-input").value;
+  
+  const newlistItem = document.getElementById('js-list-input');
+  const newItem = document.createElement('li')
+  newItem.innerHTML = input
+  newlistItem.appendChild(newItem);
   
   //get data from ul
+  // Now we are trying to build a function? to be able to always push text to list.
+  // we declare a variable called todo (this what we will send)
+  
+  
+ 
 
 
+  //toDoItems.push(todo)
   //push new data into list
 
   // push radio, edit and delete buttons into the list
@@ -18,7 +31,7 @@ function addNewFunction(){
 
 
 
-    alert(input);
+  
 
 
 }
